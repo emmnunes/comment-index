14fb61d904b7653eb508d71b258f5d174ccdc570
Eduardo Nunes <ed@eduardonunes.me>
2016-08-01 12:17:44 +0100

    Initial commit :baby:

---
+++ spec/comment-index-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import CommentIndexView from '../lib/comment-index-view';
+
+describe('CommentIndexView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
